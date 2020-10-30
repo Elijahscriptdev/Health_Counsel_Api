f557b1ced047212d66bbfed0228c33aaf94df939
Elijahscriptdev <elijahobominuru@gmail.com>
2020-10-30 23:21:12 +0100

    render data when login

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -9,15 +9,23 @@ class AuthenticationController < ApplicationController
   # end
 
   def authenticate
+
+  user = {}
+  
     if User.find_by(email: auth_params[:email])
       auth_token =
         AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+      user = User.find_by(email: auth_params[:email])
     end
     if Doctor.find_by(email: auth_params[:email])
       auth_token =
         AuthenticateDoctor.new(auth_params[:email], auth_params[:password]).call
+      user = Doctor.find_by(email: auth_params[:email])
     end
-    json_response(auth_token: auth_token)
+    json_response( response: {
+      auth_token: auth_token,
+      user: user 
+      })
   end
 
   private
